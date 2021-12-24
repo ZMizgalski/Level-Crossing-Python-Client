2adab2e375496ef162a957f8dddb7cdb26aaa3db
ZMizgalski <64747264+ZMizgalski@users.noreply.github.com>
2021-12-24 14:27:46 +0100

    update

--- ConnectToSocket.py
+++ ConnectToSocket.py
@@ -1,5 +1,6 @@
 import socket
 from time import sleep
+import threading
 
 
 class ConnectToSocket(object):
@@ -8,28 +9,31 @@ class ConnectToSocket(object):
         self.TCP_PORT = TCP_PORT
         self.CLIENT_PORT = CLIENT_PORT
         self.CONNECTED = False
+        self.CLOSE_APP = False
         self.DATA_TRANSMITTED = False
-        self.DATA = "http://" + IP + ":" + str(CLIENT_PORT)
+        self.DATA = "http://" + IP + ":" + str(CLIENT_PORT) + "\n"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.createConnection()
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.thread = threading.Thread(target=self.createConnection, args=())
+        self.thread.start()
 
     def __del__(self):
         self.sock.close()
+        sleep(1)
+        quit()
 
     def createConnection(self):
         try:
             self.sock.connect((self.HOST_IP, self.TCP_PORT))
             print("Connected")
-            sleep(1)
             if not self.DATA_TRANSMITTED:
-                self.sock.send(bytes(self.DATA + "\n", "UTF-8"))
+                self.sock.send(self.DATA.encode("UTF-8"))
                 print(self.DATA)
                 self.DATA_TRANSMITTED = True
                 self.CONNECTED = True
                 while self.CONNECTED:
-                    sleep(2)
                     try:
-                        self.sock.send(bytes('', "UTF-8"))
+                        self.sock.send(''.encode("UTF-8"))
                     except socket.error:
                         self.CONNECTED = False
                         self.DATA_TRANSMITTED = False
--- Main.py
+++ Main.py
@@ -1,7 +1,34 @@
 import ConnectToSocket
 
+from flask import Flask, render_template, Response, request
+# import VideoCamera
+#
+# pi_camera = VideoCamera.VideoCamera(flip=False)
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#
+#
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(pi_camera),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == "__main__":
     HOST = "192.168.1.212"
     TCP_PORT = 10001
     CLIENT_PORT = 8080
-    ConnectToSocket.ConnectToSocket(HOST, TCP_PORT, CLIENT_PORT)
+    sock = ConnectToSocket.ConnectToSocket(HOST, TCP_PORT, CLIENT_PORT)
+    # app.run(host='192.168.1.40', debug=False, port=8080)
